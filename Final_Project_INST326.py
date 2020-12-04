d905cd2ae9d9be476f0be1d2a00258813fb33662
Turk Hasnan <71361364+turkhasnan@users.noreply.github.com>
2020-12-04 18:18:58 -0500

    Update Final_Project_INST326.py
    Added a new method called "capacity" to check the room capacity.

--- Final_Project_INST326.py
+++ Final_Project_INST326.py
@@ -7,22 +7,22 @@ class  Name:
     
     Args:
     
-    first_name(string): first name of student
+        first_name(string): first name of student
     
-    last_name(string): last name of student
+        last_name(string): last name of student
     
-    id(int): id number of the student
+        id(int): id number of the student
     
-    covid(string): check whether or not a student has taken the covid 19 test
+        covid(string): check whether or not a student has taken the covid 19 test
     
     """
     def _init_(self, first_name, last_name, id, covid, reason, room):
         """initialize first and last name, id and covid check
         Args:
-        first_name(string): first name of student
-        last_name(string): last name of student
-        id(int): id number of the student
-        covid(string): check whether or not a student has taken the covid 19 test
+            first_name(string): first name of student
+            last_name(string): last name of student
+            id(int): id number of the student
+            covid(string): check whether or not a student has taken the covid 19 test
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -31,7 +31,7 @@ class  Name:
         self.reason = reason
         self.room = room
         
-        
+    
     def info(self):
         """prints out student id after its been input from the student 
         
@@ -44,10 +44,29 @@ class  Name:
         print(f"reason: {self.reason}")
         print(f"room #: {self.room}")
         
+        
+def capacity(roomcap):
+    """Checks the room capacity. If it is 20 people, then the room is full
+    
+    Parameters:
+        roomcap: integer value that holds the amount of students in the room
+        
+    Side effect:
+        After the message, the program quits.
+    """
+    if roomcap >= 21:
+        print("Sorry, the room is full. Please try again and pick a different room.")
+        quit()
+   
+        
 
 if __name__ == "__main__":
 
     student = Name()
+    roomcap1 = 0
+    roomcap2 = 0
+    roomcap3 = 0
+    roomcap4 = 0
     
     # Name input
     student.first_name = input("Please enter your first name: ")
@@ -96,10 +115,14 @@ if __name__ == "__main__":
         while True:
             student.room = int(input("which room would you like? 1 or 2: "))
             if student.room == 1:
+                roomcap1 = roomcap1 + 1
+                capacity(roomcap1)
                 print("This is your alloted time: ")
                 print(" ")
                 break 
             elif student.room == 2:
+                roomcap2 = roomcap2 + 1
+                capacity(roomcap2)
                 print("This is your alloted time: ")
                 print(" ")
                 break
@@ -107,10 +130,14 @@ if __name__ == "__main__":
         while True:
             student.room = int(input("which room would you like 3 or 4: "))
             if student.room == 3:
+                roomcap3 = roomcap3 + 1
+                capacity(roomcap3)
                 print("This is your alloted time: ")
                 print(" ")
                 break
             elif student.room == 4:
+                roomcap4 = roomcap4 + 1
+                capacity(roomcap4)
                 print("This is your alloted time: ")
                 print(" ")
                 break
